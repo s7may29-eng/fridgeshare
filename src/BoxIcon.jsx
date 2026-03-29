eced94cce6229c4011cbff9af5403ee068280b15
s7may29-eng <s.7.may.29@gmail.com>
2026-03-29 23:13:35 +0900

    BoxIcon.jsx を作成

---
+++ src/BoxIcon.jsx
@@ -0,0 +1,10 @@
+import { BOX_SVG } from './constants';
+
+export default function BoxIcon({ k, size = 40 }) {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: BOX_SVG[k] || BOX_SVG['fridge'] }}
+      style={{ width: size, height: size, display: 'flex', alignItems: 'center', justifyContent: 'center', flexShrink: 0 }}
+    />
+  );
+}
